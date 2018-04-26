4a58bf0eefe77cc25812212718bf4a9feaac810b
nikolay-gerasimov <31804148+nikolay-gerasimov@users.noreply.github.com>
2018-04-26 16:59:20 +0300

    started natAddFromMulFac.hs problem

---
+++ natAddFromMulFac.hs
@@ -0,0 +1,19 @@
+data Nat = Zero | Suc Nat deriving Show
+
+fromNat :: Nat -> Integer
+fromNat Zero = 0
+fromNat (Suc n) = fromNat n + 1
+
+fromInt :: Integer -> Nat
+fromInt 0 = Zero
+fromInt x = Suc $ fromInt (x-1)
+
+add :: Nat -> Nat -> Nat
+add Zero y = y
+add x Zero = x
+add (Suc x) (Suc y) = Suc (add x (Suc y))
+
+mul :: Nat -> Nat -> Nat
+mul Zero y = Zero
+mul x Zero = Zero
+mul (Suc x) (Suc y) = helper (Suc x) (Suc y) fromNat (Suc y) 
