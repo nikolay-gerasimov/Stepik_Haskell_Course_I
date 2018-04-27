8cf3c71941419d17fd40b5a95014f31ec465be95
nikolay-gerasimov <31804148+nikolay-gerasimov@users.noreply.github.com>
2018-04-27 22:38:54 +0300

    resolved treeSome.hs problem

--- treeSome.hs
+++ treeSome.hs
@@ -1,7 +1,14 @@
 data Tree a = Leaf a | Node (Tree a) (Tree a)
 
 height :: Tree a -> Int
-height = 
+height (Leaf a) = 0
+height (Node a b) = 1 + (height a) `max` (height b) 
 
 size :: Tree a -> Int
-size = undefined
+size (Leaf a) = 1
+size (Node (Leaf a) (Leaf b)) = 3
+size (Node a b) = 1 + size a + size b
+
+data Treeleaves = L | R deriving Show
+
+tree = Node (Node (Node (Leaf R) (Leaf L)) (Leaf R))(Leaf R)
