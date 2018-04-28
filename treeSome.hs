eebb2e572419608e13a156df94af7ae7ef8fba11
nikolay-gerasimov <31804148+nikolay-gerasimov@users.noreply.github.com>
2018-04-28 10:48:10 +0300

    resolved avgTree problem(file treeSome.hs)

--- treeSome.hs
+++ treeSome.hs
@@ -9,6 +9,19 @@ size (Leaf a) = 1
 size (Node (Leaf a) (Leaf b)) = 3
 size (Node a b) = 1 + size a + size b
 
+avg :: Tree Integer -> Integer
+avg t =
+    let (c,s) = go t
+    in s `div` c
+    
+go :: Tree Integer -> (Integer,Integer)
+go (Leaf a) = (1,a)
+go (Node a b) = go a `plus` go b
+
+plus :: (Num a) => (a,a) -> (a,a) -> (a,a)
+plus (a,b) (x,y) = (a+x,b+y)
+
 data Treeleaves = L | R deriving Show
 
-tree = Node (Node (Node (Leaf R) (Leaf L)) (Leaf R))(Leaf R)
+tree  = Node (Node (Node (Leaf 100) (Leaf 200)) (Leaf 201))(Leaf 303)
+tree1 = Node (Node (Node (Leaf L) (Leaf R)) (Leaf R))(Leaf R)
